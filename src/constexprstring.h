b34d4e489df6fb9dcaeeb01095d4c117e813f541
Jan Lanik <980571+jlanik@users.noreply.github.com>
2021-10-02 14:59:24 +0200

    First version of strcmp
    Doesn't work fully in constexpr context.

--- src/constexprstring.h
+++ src/constexprstring.h
@@ -9,4 +9,46 @@ constexpr std::size_t strlen(const char *str) {
   }
   return cnt;
 }
+
+constexpr int strcmp(const char *lhs, const char *rhs) {
+  constexpr int LEFT_LOWER{-1};
+  constexpr int LEFT_GREATER{1};
+  constexpr int BOTH_EQUAL{0};
+
+  // This copy is necessary as lhs and rhs can not be modified in constexpr
+  // context.
+  char const *lptr = lhs;
+  char const *rptr = rhs;
+
+  do {
+    if ('\0' == *lptr) {
+      if ('\0' == *rptr) {
+        // both empty
+        return BOTH_EQUAL;
+      } else {
+        // lptr is a prefix of rptr
+        return LEFT_LOWER;
+      }
+    } else {
+      if ('\0' == *rptr) {
+        // rptr is a prefix of lptr
+        return LEFT_GREATER;
+      } else {
+        int const diff = (static_cast<int>(static_cast<unsigned char>(*lptr)) -
+                          static_cast<int>(static_cast<unsigned char>(*rptr)));
+        if (diff > 0) {
+          return LEFT_GREATER;
+        } else if (diff < 0) {
+          return LEFT_LOWER;
+        } else {
+          // Equal until now - need to look at the next symbol.
+          ++lptr;
+          ++rptr;
+          continue;
+        }
+      }
+    }
+
+  } while (true);
+}
 }
--- src/test_constexprstring.cpp
+++ src/test_constexprstring.cpp
@@ -21,6 +21,58 @@ TEST(StrlenTest, NonEmpty) {
 }
 
 // strcmp
+TEST(StrCmpTest, Empty) {
+  constexpr const char *str_empty1 = "";
+  constexpr const char *str_empty2 = "";
+  constexpr const char *str_nonempty = "HelloWorld!";
+  static_assert(cxs::strcmp(str_empty1, str_empty2) == 0, "Test Failure");
+  EXPECT_EQ(0, cxs::strcmp(str_empty1, str_empty2));
+  EXPECT_EQ(std::strcmp(str_empty1, str_empty2),
+            cxs::strcmp(str_empty1, str_empty2));
+
+  static_assert(-1 == cxs::strcmp(str_empty1, str_nonempty), "Test Failure");
+  EXPECT_EQ(-1, cxs::strcmp(str_empty1, str_nonempty));
+  EXPECT_EQ(std::strcmp(str_empty1, str_nonempty),
+            cxs::strcmp(str_empty1, str_nonempty));
+
+  static_assert(1 == cxs::strcmp(str_nonempty, str_empty1), "Test Failure");
+  EXPECT_EQ(1, cxs::strcmp(str_nonempty, str_empty1));
+  EXPECT_EQ(std::strcmp(str_nonempty, str_empty1),
+            cxs::strcmp(str_nonempty, str_empty1));
+}
+
+TEST(StrCmpTest, OneCharSame) {
+  constexpr const char *str1 = "H";
+  constexpr const char *str2 = "H";
+  // static_assert(0 == cxs::strcmp(str1, str2), "Test Failure");
+  EXPECT_EQ(0, cxs::strcmp(str1, str2));
+  EXPECT_EQ(std::strcmp(str1, str2), cxs::strcmp(str1, str2));
+}
+TEST(StrCmpTest, OneCharDifferent) {
+  constexpr const char *str1 = "H";
+  constexpr const char *str2 = "X";
+  // static_assert(-1 == cxs::strcmp(str1, str2), "Test Failure");
+  EXPECT_EQ(-1, cxs::strcmp(str1, str2));
+  // static_assert(1 == cxs::strcmp(str2, str1), "Test Failure");
+  EXPECT_EQ(1, cxs::strcmp(str2, str1));
+}
+
+TEST(StrCmpTest, Same) {
+  constexpr const char *str1 = "Hi There!";
+  constexpr const char *str2 = "Hi There!";
+  EXPECT_EQ(0, cxs::strcmp(str1, str2));
+}
+
+TEST(StrCmpTest, Prefix) {
+  constexpr const char *str1 = "Hi";
+  constexpr const char *str2 = "Hi There!";
+  EXPECT_EQ(-1, cxs::strcmp(str1, str2));
+  EXPECT_EQ(1, cxs::strcmp(str2, str1));
+}
+
+TEST(StrCmpTest, DifferentFirstChar) {}
+
+TEST(StrCmpTest, DifferentLaterChar) {}
 
 // strncmp
 
